1398d2537dd4bb4c94027b78ffae413967e2a9ee
edowaye <edowayeadenaiya@gmail.com>
2022-10-16 07:09:01 -0700

    Included a main.h file that will contain all prototype functions to be used in team project printf

---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+  *main: header files for prototypes
+  *
+  *Description - Inclusion of prototype functions
+  for team project printf
+  *
+  */
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+
+#endif /*MAIN_H*/
